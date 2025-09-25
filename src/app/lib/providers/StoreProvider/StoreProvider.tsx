5e2594461140f1e1a21904ba109cf1c6c0962517
brandonhsz <brandonhszzz@gmail.com>
2025-09-25 11:27:23 -0600

    feat: implement Redux store setup with custom hooks and OpenAPI slice

---
+++ src/app/lib/providers/StoreProvider/StoreProvider.tsx
@@ -0,0 +1,21 @@
+'use client'
+import { useRef } from 'react'
+import { Provider } from 'react-redux'
+import { makeStore, AppStore } from '@/app/lib/store'
+import { initializeMessageApi } from '@/app/lib/store/openApi'
+
+
+export default function StoreProvider({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  const storeRef = useRef<AppStore>(undefined)
+  if (!storeRef.current) {
+    // Create the store instance the first time this renders
+    storeRef.current = makeStore()
+    storeRef.current.dispatch(initializeMessageApi("Procesando..."))
+  }
+
+  return <Provider store={storeRef.current}>{children}</Provider>
+}
